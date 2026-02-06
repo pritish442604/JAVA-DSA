64147b2d00276e41a419bac928ffed9e3138af0d
Pritish Mandal <pritishmandal21@gmail.com>
2026-02-06 09:27:33 +0530

    DSA

--- arthmatic.java
+++ arthmatic.java
@@ -4,7 +4,7 @@ public class arthmatic {
     public static void main(String[] args) {
         int a =10;
         int b = a--;
-        System.out.println(a);
+        System.out.println(a);//hiiii
         System.out.println(b);
     }
 }
