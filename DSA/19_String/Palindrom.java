3f48a4fade489207e335508fba662bd8d3bc2f53
Pritish Mandal <pritishmandal21@gmail.com>
2026-03-09 22:05:00 +0530

    Revision function method

--- DSA/19_String/Palindrom.java
+++ DSA/19_String/Palindrom.java
@@ -2,7 +2,7 @@ public class Palindrom {
     public static boolean palindrom(String str) {
         for (int i = 0; i <str.length()/2 ; i++) {// ye loop string ke first half tak chalega
             int n=str.length();// string ke length ko n me store kar liya kyuki hume last character ko access karna hai
-            if(str.charAt(i)!=str.charAt(n-1-i)){// its not a palindrom     
+            if(str.charAt(i)!=str.charAt(n-1-i)){// its not a palindrom    
 
              
             return false; 
