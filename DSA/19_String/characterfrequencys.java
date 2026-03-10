02930d38d3282f716e3bc31dc4c4cc3f4efec99e
Pritish Mandal <pritishmandal21@gmail.com>
2026-03-10 23:19:05 +0530

    first commit

---
+++ DSA/19_String/characterfrequencys.java
@@ -0,0 +1,16 @@
+ public class characterfrequencys {
+    public static void main(String[] args) {
+        String str = "banana";
+        int[] freq = new int[256];
+
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] != 0) {
+                System.out.println((char) i + " : " + freq[i]);// print character and its frequency
+            }
+        }
+    }
+} 
